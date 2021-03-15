738e9edb1d7d6795c6ccfd933020e568164a231f
Ryuseisukisugite <2020m092@kuas.ac.jp>
2021-03-15 12:26:29 +0900

    log up のボタン
    login に戻るボタン設置

--- app/src/main/java/com/example/akikomi/LogUpActivity.java
+++ app/src/main/java/com/example/akikomi/LogUpActivity.java
@@ -2,7 +2,11 @@ package com.example.akikomi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.akikomi.ui.login.LoginActivity;
 
 public class LogUpActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class LogUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_up);
+    }
+
+    public void goToLogin(View view) {
+        Intent toLogin = new Intent(this, LoginActivity.class);
+        startActivity(toLogin);
     }
 }
